581dc32a6c35d9a8c8196267e4967953c205e99f
Damon <nichold5@mail.uc.edu>
2020-09-14 23:30:32 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,54 @@
+import java.util.Scanner;
+class Main {
+  public static void main(String[] args){ 
+
+{
+Scanner n = new Scanner(System.in);
+
+int number;
+System.out.println("Enter a whole number to count from:");
+number= n.nextInt();
+  if (number >= 0)
+    while(number > 0)
+    {
+    number --;
+    System.out.println(number);
+
+
+    }
+  else if (number <= 0)
+    while (number <0)
+    {
+      number ++;
+
+      System.out.println(number);
+    }
+
+
+       System.out.println("Team Rocket is blasting off again!");
+}
+
+}
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
